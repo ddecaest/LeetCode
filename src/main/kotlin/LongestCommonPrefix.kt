6d569dc41510e9187d1073bb7445a7815c2ebc7b
dieter decaestecker <dieterd1992@gmail.com>
2023-12-23 21:18:11 +0100

    First solution

---
+++ src/main/kotlin/LongestCommonPrefix.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    val test = arrayOf("flower", "flow", "flowery", "flightery", "flight")
+    println(Solution.longestCommonPrefix(test))
+}
+
+object Solution {
+
+    fun longestCommonPrefix(strs: Array<String>): String {
+        strs.sort()
+
+        val first = strs[0]
+        val last = strs[strs.size - 1]
+        for (i in first.length downTo 0 step 1) {
+            val firstSubString = first.substring(0, i)
+            if (last.startsWith(firstSubString)) {
+                return firstSubString
+            }
+        }
+        return ""
+    }
+}
